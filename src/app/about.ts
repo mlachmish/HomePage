c9ae2d0759b65851ded2a6f8ccfb6ae838af1f4f
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-07-18 22:49:08 +0300

    Add CV link

--- src/app/about.ts
+++ src/app/about.ts
@@ -10,4 +10,9 @@ import {Component} from '@angular/core';
 })
 export class About {
 
+    openCV() {
+        var win = window.open("https://firebasestorage.googleapis.com/v0/b/homepage-bd6f3.appspot.com/o/Matan%20Lachmish%20-%20CV.pdf?alt=media&token=eed63f35-aff7-4a16-9ca2-c3c2d012e43f", '_blank');
+        win.focus();
+    }
+
 }

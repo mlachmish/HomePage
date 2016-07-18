79d61853e0ddfd79d9101ac990c8918e82a36577
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-07-19 00:36:01 +0300

    Integrate with formspree.io

--- src/app/contact.ts
+++ src/app/contact.ts
@@ -1,10 +1,39 @@
 /**
  * Created by matan on 15/07/2016.
  */
-import {Component} from '@angular/core';
+import {Injectable, Inject, Component} from '@angular/core';
+import {Http, HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
     selector: 'ContactComponent',
-    template: require('./contact.html')
+    template: require('./contact.html'),
+    providers: [HTTP_PROVIDERS]
 })
-export class Contact {}
+
+@Injectable()
+export class Contact {
+
+    constructor(@Inject(Http) public http: Http) {
+        this.http = http;
+    }
+
+    sendMessage() {
+        var name = (<HTMLInputElement>document.getElementById('contact-name')).value;
+        var email = (<HTMLInputElement>document.getElementById('contact-email')).value;
+        var message = (<HTMLTextAreaElement>document.getElementById('contact-message')).value;
+        var subject = 'Website contact';
+
+        var body = {
+            name: JSON.stringify(name),
+            email: JSON.stringify(email),
+            message: JSON.stringify(message),
+            subject: JSON.stringify(subject)
+        };
+
+        this.http.post('//formspree.io/' + 'matanlachmish' + '@' + 'gmail.com', body, {})
+            .subscribe(res => {
+                alert('I will be in touch :)');
+            });
+    }
+
+}

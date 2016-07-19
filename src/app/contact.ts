f2b35197f55dfb70b989a0d10929229fe7dc9d80
Matan Lachmish <matan.lachmish@jivesoftware.com>
2016-07-19 14:03:36 +0300

    Contact social logo animation

--- src/app/contact.ts
+++ src/app/contact.ts
@@ -13,6 +13,51 @@ import {Http, HTTP_PROVIDERS} from '@angular/http';
 @Injectable()
 export class Contact {
 
+    private linkedinLogoWhite: string = '../img/social/white/linkedin-logo.png';
+    private linkedinLogoColor: string = '../img/social/color/linkedin-logo.png';
+
+    private githubLogoWhite: string = '../img/social/white/github-logo.png';
+    private githubLogoColor: string = '../img/social/color/github-logo.png';
+
+    private facebookLogoWhite: string = '../img/social/white/facebook-logo.png';
+    private facebookLogoColor: string = '../img/social/color/facebook-logo.png';
+
+    private twitterLogoWhite: string = '../img/social/white/twitter-logo.png';
+    private twitterLogoColor: string = '../img/social/color/twitter-logo.png';
+
+    public linkedinLogo: string = this.linkedinLogoWhite;
+    public githubLogo: string = this.githubLogoWhite;
+    public facebookLogo: string = this.facebookLogoWhite;
+    public twitterLogo: string = this.twitterLogoWhite;
+
+    private mouseOverLinkedin() {
+        this.linkedinLogo = this.linkedinLogoColor;
+    };
+    private mouseOutLinkedin() {
+        this.linkedinLogo = this.linkedinLogoWhite;
+    };
+
+    private mouseOverGithub() {
+        this.githubLogo = this.githubLogoColor;
+    };
+    private mouseOutGithub() {
+        this.githubLogo = this.githubLogoWhite;
+    };
+
+    private mouseOverFacebook() {
+        this.facebookLogo = this.facebookLogoColor;
+    };
+    private mouseOutFacebook() {
+        this.facebookLogo = this.facebookLogoWhite;
+    };
+
+    private mouseOverTwitter() {
+        this.twitterLogo = this.twitterLogoColor;
+    };
+    private mouseOutTwitter() {
+        this.twitterLogo = this.twitterLogoWhite;
+    };
+
     constructor(@Inject(Http) public http: Http) {
         this.http = http;
     }
